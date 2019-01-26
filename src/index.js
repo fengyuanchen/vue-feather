f538b12e67670fa34868fb8e10676f054b16dd11
Chen Fengyuan <chenfengyuan@outlook.com>
2019-01-26 13:47:51 +0800

    feat: register the component automatically once loaded

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ Feather.install = (Vue) => {
   Vue.component(Feather.name, Feather);
 };
 
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(Feather);
+}
+
 export default Feather;
